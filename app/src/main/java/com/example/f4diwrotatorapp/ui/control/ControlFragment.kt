167a7ebe32fa2228470468457fc0f901519c073a
F4DIW <f4diwqsll@gmail.com>
2026-05-26 16:41:47 +0200

    feat: add remaining stubs

---
+++ app/src/main/java/com/example/f4diwrotatorapp/ui/control/ControlFragment.kt
@@ -0,0 +1,2 @@
+package com.example.f4diwrotatorapp.ui.control
+// TODO: Control screen fragment (compass, AZ/EL gauges, GO/STOP/PARK)
---
+++ app/src/main/java/com/example/f4diwrotatorapp/ui/control/ControlViewModel.kt
@@ -0,0 +1,2 @@
+package com.example.f4diwrotatorapp.ui.control
+// TODO: ControlViewModel - bridge between UI and RotatorRepository
